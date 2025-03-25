0811f248deeaa351facfa84ac1314c5fa8c796c3
Noe Priss Definel <ndefinel0@gmail.com>
2025-03-25 11:58:15 +0100

    Ajout des fonctions de clalculs basiques

---
+++ script.js
@@ -0,0 +1,29 @@
+function add(a, b) {
+
+    return a + b;
+};
+
+function soustract(a, b) {
+
+    return a - b;
+};
+
+function multiply(a, b) {
+
+    return a * b;
+};
+
+function divide(a, b) {
+
+    if (b === 0) {
+
+        return 'We Cant divide by zero'
+    }
+
+    return a / b;
+};
+
+
+
+
+
